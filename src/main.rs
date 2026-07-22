80dca36c5d110780f68f280cfdca72ff940ef7b2
Mike Weber <absurd.potato.inc@gmail.com>
2026-07-22 02:05:52 -0400

    basic shenanigans

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,20 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn(Text::new("Hellow World!"));
 }
 
 #[derive(Resource)]
 struct GreetTimer(Timer);
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>, mut commands: Commands) {
     // update our timer with the time elapsed since the last update
     // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
             println!("hello {}!", name.0);
         }
+        commands.spawn((Person, Name("Bumbleshit Fartkin".to_string())));
     }
 }
 
---
+++ src/map/mod.rs
@@ -0,0 +1,4 @@
+struct Plane {
+    normal: Vec3<f32>,
+    distance: f32,
+}
